658cb26df05d1fdd2c1c1a7a2ae51860438ac120
Yvan Tchuente <yvantchuente@gmail.com>
2023-05-15 23:25:58 +0100

    build: set the default font
    - Specify "Inter" font family fonts in app.css
    - Customize the tailwind's "sans" font family configuration to add
      "Inter" as first font
    
    In order to set the Inter font as the default font and specify its
    variants for specific uses cases.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-    ],
+    content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
     theme: {
+        fontFamily: {
+            sans: ["Inter", ...defaultTheme.fontFamily.sans],
+        },
         extend: {},
     },
     plugins: [],
