ef3c23ef5f256a939654f5dc0744d0914baed235
Yvan Tchuente <yvantchuente@gmail.com>
2023-05-15 23:25:58 +0100

    Set up database seeders to run tests
    Define database seeder classes to seed the database with dummy data
    to use when running tests

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,8 +2,9 @@
 
 namespace Database\Factories;
 
+use Illuminate\Support\Facades\Hash;
+use App\Enumerations\FieldOfStudy;
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -17,12 +18,23 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        foreach (FieldOfStudy::cases() as $field) {
+            $fields[] = $field->value;
+        }
+        $rand_key = array_rand($fields);
+
         return [
-            'name' => fake()->name(),
+            'username' => fake()->userName(),
+            'password' => Hash::make('W5$FY&L9l8k9LsJ'),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
+            'batch' => fake()->year(),
+            'field_of_study' => $fields[$rand_key],
+            'country' => fake()->country(),
+            'city' => fake()->city(),
+            'phone' => fake()->phoneNumber(),
+            'avatar' => '/images/graphics/default-profile-picture.png',
         ];
     }
 
--- database/seeders/NewsArticleSeeder.php
+++ database/seeders/NewsArticleSeeder.php
@@ -2,22 +2,19 @@
 
 namespace Database\Seeders;
 
+use App\Models\NewsArticle;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-class DatabaseSeeder extends Seeder
+class NewsArticleSeeder extends Seeder
 {
     use WithoutModelEvents;
 
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      */
     public function run(): void
     {
-        $this->call([
-            UserSeeder::class,
-            EventSeeder::class,
-            NewsArticleSeeder::class,
-        ]);
+        NewsArticle::factory(10)->create();
     }
 }
