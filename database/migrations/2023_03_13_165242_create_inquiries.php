73475e06983fcd8e123633e69c9c0a858467381d
Yvan Tchuente <yvantchuente@gmail.com>
2023-05-15 23:25:57 +0100

    Define the database schema definition

---
+++ database/migrations/2023_03_13_165242_create_inquiries.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inquiries', function (Blueprint $table) {
+            $table->id();
+            $table->text('message');
+            $table->string('author_name');
+            $table->string('author_email');
+            $table->string('author_phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inquiries');
+    }
+};
